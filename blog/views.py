5118bd83622872d32bddb657da0a19e6814d8f1f
xiaozhiqi2016 <329275108@qq.com>
2016-07-10 22:30:17 +0800

    获取导航栏的数据从views通过模板语言传给前端for循环展示,获取广告数据也是同样的

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@
 import logging
 from django.shortcuts import render
 from django.conf import settings  #需要导入这个模块
-from blog.models import Category
+from blog.models import *
 
 logger = logging.getLogger('blog.views')
 
@@ -18,13 +18,18 @@ def global_setting(request):
     'WEIBO_TENCENT' : settings.WEIBO_TENCENT,
     'PRO_RSS' : settings.PRO_RSS,
     'PRO_EMAIL' : settings.PRO_EMAIL,
+    'MEDIA_URL' : settings.MEDIA_URL
     }
 
 
 def index(request):
     try:
+        # 分类信息获取(导航数据)
         category_list = Category.objects.all()
+        # 广告数据
+        ad_list = Ad.objects.all()
+        # 最新文章数据
     except Exception as e:
         logger.error(e)
-
-    return render(request,'index.html',{'category_list':category_list})
+    result = {'category_list':category_list,'ad_list':ad_list}
+    return render(request,'index.html',result)
