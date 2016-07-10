b64ebedf86ea44db879350282d460b3d1021a7c7
xiaozhiqi2016 <329275108@qq.com>
2016-07-10 14:28:55 +0800

    admin的使用,把model的类向admin里面注册,并提供了自定义的注册方式和默认注册方式,默认就是默认的注册方式,继承了admin.ModelAdmin这个类时可以使用自定义的注册方式,具体查看代码

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,39 @@
+#coding:utf-8
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+
+# fields\exclude  只显示定义的字段\除掉这些字段
+# fieldsets       进行对字段进行分类设置
+# list_display    列出可显示的内容
+# list_display_links   列出可显示的内容可以进行点击链接
+# list_editable   在列表中设置可以编辑的状态
+# list_filter
+# inlines
+
+
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'desc','click_count',)
+    list_display_links = ('title', 'desc',)
+    list_editable = ('click_count',)
+#
+#
+#     fieldsets = (
+#         (None, {
+#             'fields': ('title', 'desc', 'content')
+#         }),
+#         ('高级设置', {
+#             'classes': ('collapse',),
+#             'fields': ('click_count', 'is_recommend',)
+#         }),
+#     )
+
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Links)
+admin.site.register(Ad)
+
